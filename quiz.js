a35dcefec3f3dc5cfa34ddf03f076e50056d48af
Doris Pasic <doris.pasic@dci-student.org>
2024-10-08 14:44:37 +0200

    am quiz rumgetüftelt

--- quiz.js
+++ quiz.js
@@ -7,86 +7,100 @@ let questions = [
         answers: ["2", "8", "4", "6"], // = Array
         correctAnswer: 1,  // Integer, der richtige Index ist 1 ( 8 Beine ),
         description: `Spinnen haben immer acht Beine und zwei Körperglieder, Insekten hingegen haben immer sechs Beine und drei Körperglieder.`
+        //Bild von <a href="https://pixabay.com/de/users/alba1970-29824635/?utm_source=link-attribution&utm_medium=referral&utm_campaign=image&utm_content=8260620">alba1970</a> auf <a href="https://pixabay.com/de//?utm_source=link-attribution&utm_medium=referral&utm_campaign=image&utm_content=8260620">Pixabay</a>
     },
     {
         question: "Zu welcher Tierart gehört die Rasse 'Maine Coon'?",
         answers: ["Katze", "Hund", "Pferd", "Kaninchen"],
-        correctAnswer: 0   
-        // Die Maine Coon ist die größte und schwerste Katzenrasse der Welt und liebt es draußen in der Natur zu sein. Sie wird auch als „Sanfter Riese“ bezeichnet. 
+        correctAnswer: 0  , 
+        description: `Die Maine Coon ist die größte und schwerste Katzenrasse der Welt und liebt es draußen in der Natur zu sein. Sie wird auch als „Sanfter Riese“ bezeichnet. `
+        //Bild von <a href="https://pixabay.com/de/users/miezekieze-607096/?utm_source=link-attribution&utm_medium=referral&utm_campaign=image&utm_content=7954384">miezekieze</a> auf <a href="https://pixabay.com/de//?utm_source=link-attribution&utm_medium=referral&utm_campaign=image&utm_content=7954384">Pixabay</a>
     },
     {
         question: "Welcher der folgenden Vögel kann nicht fliegen?",
         answers: ["Rabe", "Strauß", "Storch", "Elster"],
-        correctAnswer:  1
-        // Der Strauß kann zwar nicht fliegen, aber dafür kann er eine halbe Stunde lang mit 50 Kilometern pro Stunde laufen. So schnell dürfen bei uns teilweise die Autos in den Städten fahren! Während kürzerer Zeit schafft er sogar 70 Stundenkilometer.
+        correctAnswer:  1,
+        description: `Der Strauß kann zwar nicht fliegen, aber dafür kann er eine halbe Stunde lang mit 50 Kilometern pro Stunde laufen. So schnell dürfen bei uns teilweise die Autos in den Städten fahren! Während kürzerer Zeit schafft er sogar 70 Stundenkilometer.`
+        //Bild von <a href="https://pixabay.com/de/users/valeriebaron-248388/?utm_source=link-attribution&utm_medium=referral&utm_campaign=image&utm_content=341989">Valérie BARON</a> auf <a href="https://pixabay.com/de//?utm_source=link-attribution&utm_medium=referral&utm_campaign=image&utm_content=341989">Pixabay</a>
     },    
     {
         question: "Welches Tier ist ein Beuteltier?",
         answers: ["Orangutan", "Elefant", "Erdmännchen", "Koala"],
-        correctAnswer:  3
-        // Koalas sind nachtaktive Beuteltiere, die in Australien in Eukalyptusbäumen leben und nur Eukalyptus-Blätter und -Rinde frisst. Der Koala ist eng verwandt mit den Kängurus, sie sind beide die wichtigsten Symbole Australiens.
+        correctAnswer:  3,
+        description: `Koalas sind nachtaktive Beuteltiere, die in Australien in Eukalyptusbäumen leben und nur Eukalyptus-Blätter und -Rinde frisst. Der Koala ist eng verwandt mit den Kängurus, sie sind beide die wichtigsten Symbole Australiens.`
+        //Bild von <a href="https://pixabay.com/de/users/picsbyannyk-5112090/?utm_source=link-attribution&utm_medium=referral&utm_campaign=image&utm_content=3055832">Annick Vanblaere</a> auf <a href="https://pixabay.com/de//?utm_source=link-attribution&utm_medium=referral&utm_campaign=image&utm_content=3055832">Pixabay</a>
     },    
     {
         // 5.
         question: "Eine Biene ist ein ...",
         answers: ["Vogel", "Fisch", "Insekt", "Säugetier"],
-        correctAnswer:  2 
-        // Bienen gehören zu den Insekten und haben sechs Beine, vier Flügel und einen Panzer.
+        correctAnswer:  2 ,
+        description: `Bienen gehören zu den Insekten und haben sechs Beine, vier Flügel und einen Panzer.`
+        //Bild von <a href="https://pixabay.com/de/users/iupac-35785262/?utm_source=link-attribution&utm_medium=referral&utm_campaign=image&utm_content=8320764">Iupac</a> auf <a href="https://pixabay.com/de//?utm_source=link-attribution&utm_medium=referral&utm_campaign=image&utm_content=8320764">Pixabay</a>
     },    
     {
         question: "Ein Tausendfüßler hat durchschnittlich etwa wieviele Füße?",
         answers: ["ca. 400", "ca. 30", "ca. 1.000", "ca. 30.000"],
-        correctAnswer:  0
-        // Je nach Art besitzen sie zwischen acht und maximal 340 Beinpaare, also insgesamt höchstens 680 Beine.
+        correctAnswer:  0,
+        description: `Je nach Art besitzen sie zwischen acht und maximal 340 Beinpaare, also insgesamt höchstens 680 Beine.`
+        //Bild von <a href="https://pixabay.com/de/users/jggrz-7998824/?utm_source=link-attribution&utm_medium=referral&utm_campaign=image&utm_content=5081974">Jürgen</a> auf <a href="https://pixabay.com/de//?utm_source=link-attribution&utm_medium=referral&utm_campaign=image&utm_content=5081974">Pixabay</a>
     },    
     {
         question: `Was ist eine "Schwarze Witwe"?`,
         answers: ["Vogel", "Spinne", "Fliege", "Eidechse"],
-        correctAnswer:  1
-        // Die schwarze Witwe ist eine kleine Spinne. Ihr Durchmesser (mit Beinen gemessen) beträgt etwa 4 cm, die Körperlänge nur 1 cm. 
+        correctAnswer:  1,
+        description: `Die schwarze Witwe ist eine kleine Spinne. Ihr Durchmesser (mit Beinen gemessen) beträgt etwa 4 cm, die Körperlänge nur 1 cm. `
+        //Bild von <a href="https://pixabay.com/de/users/jgiammatteo-5271279/?utm_source=link-attribution&utm_medium=referral&utm_campaign=image&utm_content=2282084">jgiammatteo</a> auf <a href="https://pixabay.com/de//?utm_source=link-attribution&utm_medium=referral&utm_campaign=image&utm_content=2282084">Pixabay</a>
     },    
     {
         question: "Wie schlafen Giraffen?",
         answers: ["im sitzen", "im liegen", "auf dem Rücken", "im stehen"],
-        correctAnswer:  3
-        // Giraffen schlafen immer nur ein paar Minuten am Stück, insgesamt nur wenige Stunden am Tag - im Stehen.
+        correctAnswer:  3,
+        description: `Giraffen schlafen immer nur ein paar Minuten am Stück, insgesamt nur wenige Stunden am Tag - im Stehen.`
+        //Bild von <a href="https://pixabay.com/de/users/pen_ash-5526837/?utm_source=link-attribution&utm_medium=referral&utm_campaign=image&utm_content=8435321">Penny</a> auf <a href="https://pixabay.com/de//?utm_source=link-attribution&utm_medium=referral&utm_campaign=image&utm_content=8435321">Pixabay</a>
     },    
     {
         question: "Welcher Vogel baut kein eigenes Nest?",
         answers: ["Kuckuck", "Spatz", "Papagei", "Elster"],
-        correctAnswer:  0
-        // Das ist richtig! Der Kuckuck baut kein eigenes Nest, sondern legt seine Eier in fremde Nester. Sein Junges schlüpft früher als die der anderen Vögel und wirft dann die «Konkurrenz» aus dem Nest.
+        correctAnswer:  0,
+        description: `Der Kuckuck baut kein eigenes Nest, sondern legt seine Eier in fremde Nester. Sein Junges schlüpft früher als die der anderen Vögel und wirft dann die «Konkurrenz» aus dem Nest.`
+        //https://www.mein-schoener-garten.de/tiere/voegel/kuckuck/kuckuck
     },    
     {
         // 10.
         question: "Welche Art von Augen besitzen Insekten?",
         answers: ["Stäbchenaugen", "Facettenaugen", "Zäpfchenaugen", "Molchaugen"],
-        correctAnswer:  1
-        // Das ist richtig! Insekten haben Facettenaugen. Sie sind aus hunderten Einzelaugen zusammengesetzt. Aus all diesen Einzelbildern wird im Gehirn das Gesamtbild gebildet.
+        correctAnswer:  1,
+        description: `Insekten haben Facettenaugen. Sie sind aus hunderten Einzelaugen zusammengesetzt. Aus all diesen Einzelbildern wird im Gehirn das Gesamtbild gebildet.`
+        //Bild von <a href="https://pixabay.com/de/users/nguyencuong_hp-14344482/?utm_source=link-attribution&utm_medium=referral&utm_campaign=image&utm_content=4970644">Cuong Nguyen</a> auf <a href="https://pixabay.com/de//?utm_source=link-attribution&utm_medium=referral&utm_campaign=image&utm_content=4970644">Pixabay</a>
     },    
     {
         question: "Welches ist das größte aktuell lebende Säugetier?",
-        answers: ["Blauwahl", "Elefant", "Giraffe", "Buckelwal"],
-        correctAnswer:  0
-        // Das ist richtig! Der Blauwal ist das grösste Säugetier. Er kann bis zu 31 Meter lang werden und ist mehr als 100 Tonnen schwer.
+        answers: ["Blauwal", "Elefant", "Giraffe", "Buckelwal"],
+        correctAnswer:  0,
+        description: `Der Blauwal ist das grösste Säugetier. Er kann bis zu 31 Meter lang werden und ist mehr als 100 Tonnen schwer.`
+        //Bild von <a href="https://pixabay.com/de/users/three-shots-3936226/?utm_source=link-attribution&utm_medium=referral&utm_campaign=image&utm_content=2051760">Three-shots</a> auf <a href="https://pixabay.com/de//?utm_source=link-attribution&utm_medium=referral&utm_campaign=image&utm_content=2051760">Pixabay</a>
     },    
     {
         question: "Was ist eine Meerkatze?",
         answers: ["Eine Raubkatze, die im Meer Fische fängt", "Eine besondere Affenart", "Eine Raubkatze, die Meerwasser trinkt", "Eine besondere Raubkatzenart"],
-        correctAnswer:  1
-        // Das ist richtig! Eine Meerkatze ist eine Affenart, die in verschiedenen Regionen Afrikas lebt.
+        correctAnswer:  1,
+        description: `Eine Meerkatze ist eine Affenart, die in verschiedenen Regionen Afrikas lebt.`
+        //Bild von <a href="https://pixabay.com/de/users/nduati-6204972/?utm_source=link-attribution&utm_medium=referral&utm_campaign=image&utm_content=4017357">nduati</a> auf <a href="https://pixabay.com/de//?utm_source=link-attribution&utm_medium=referral&utm_campaign=image&utm_content=4017357">Pixabay</a>
     },    
     {
         question: "Welches Raubtier lebt sowohl in Europa, als auch in Asien und Amerika?",
         answers: ["Jaguar", "Luchs", "Wolf", "Fuchs"],
-        correctAnswer:  1
-        // Der Luchs ist auf 3 Kontinenten zu Hause: in Europa, Asien und in Nordamerika. In Europa steht er unter Naturschutz. Hier ist z.B. in Skandinavien der Nordluchs beheimatet. Auch in Südosteuropa streifen vereinzelt Luchse durch die Wälder.
+        correctAnswer:  1,
+        description: `Der Luchs ist auf 3 Kontinenten zu Hause: in Europa, Asien und in Nordamerika. In Europa steht er unter Naturschutz. Hier ist z.B. in Skandinavien der Nordluchs beheimatet. Auch in Südosteuropa streifen vereinzelt Luchse durch die Wälder.`
+        //Bild von <a href="https://pixabay.com/de/users/venomdesign-5201978/?utm_source=link-attribution&utm_medium=referral&utm_campaign=image&utm_content=4940240">Markus Wittmann</a> auf <a href="https://pixabay.com/de//?utm_source=link-attribution&utm_medium=referral&utm_campaign=image&utm_content=4940240">Pixabay</a>
     },    
     {
         question: "Welches ist das langsamste Säugetier?",
         answers: ["Lama", "Koalabär", "Faultier", "Schnecke"],
-        correctAnswer:  2
-        // Das ist richtig! Das Faultier bewegt sich mit 0.12 km/h vorwärts, also fast in Zeitlupe, und ist damit das langsamste Säugetier.
+        correctAnswer:  2,
+        description: `Das Faultier bewegt sich mit 0.12 km/h vorwärts, also fast in Zeitlupe, und ist damit das langsamste Säugetier.`
+        //Bild von <a href="https://pixabay.com/de/users/camejex-21495458/?utm_source=link-attribution&utm_medium=referral&utm_campaign=image&utm_content=7269590">Eddy Camejo</a> auf <a href="https://pixabay.com/de//?utm_source=link-attribution&utm_medium=referral&utm_campaign=image&utm_content=7269590">Pixabay</a>
     },    
     {
         // 15.
